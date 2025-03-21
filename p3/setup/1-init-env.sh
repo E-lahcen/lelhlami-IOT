85e03e09eabc748f9e9ac3df21e30f2fff5487a3
E-lahcen <lelhlami@student.1337.ma>
2025-03-21 08:21:30 +0000

    Update p3

--- p3/setup/1-init-env.sh
+++ p3/setup/1-init-env.sh
@@ -1,22 +1,52 @@
-# Install k3d
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+#Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+BLUE='\033[0;34m'
+
 
 # Install docker
-sudo apt update
-sudo apt install docker.io
-
-# Create a k3d cluster
-sudo k3d cluster create lelhlami --port 8080:80@loadbalancer --port 8443:443@loadbalancer
-
-# List k3d clusters
-sudo k3d cluster list
+if ! [ -x "$(command -v docker)" ]; then
+    echo "${BLUE}Installing docker${NC}"
+    sudo apt update
+    sudo apt install docker.io
+else
+    echo "${GREEN}docker is already installed${NC}"
+fi
+# Install k3d
+if ! [ -x "$(command -v k3d)" ]; then
+    echo "${BLUE}Installing k3d${NC}"
+    curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+else
+    echo "${GREEN}k3d is already installed${NC}"
+fi
 
 # Download and install kubectl from the official Kubernetes release
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-echo "$(<kubectl.sha256) kubectl" | sha256sum --check
-chmod +x kubectl
-sudo mv ./kubectl /usr/local/bin
+if ! [ -x "$(command -v kubectl)" ]; then
+    echo "${BLUE}Downloading and installing kubectl${NC}"
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+    echo "$(<kubectl.sha256) kubectl" | sha256sum --check
+    chmod +x kubectl
+    sudo mv ./kubectl /usr/local/bin
+else
+    echo "${GREEN}kubectl is already installed${NC}"
+fi
+
+# Create a k3d cluster
+echo "${BLUE}Creating a k3d cluster${NC}"
+#only create a new cluster if it does not exist
+if [ -z "$(sudo k3d cluster list | grep lelhlami)" ]; then
+    sudo k3d cluster create lelhlami --agents 2
+    # sudo k3d cluster create lelhlami --port 8081:80@loadbalancer --port 8443:443@loadbalancer
+else
+    echo "${GREEN}The cluster already exists${NC}"
+fi
+
+# List k3d clusters
+echo "${BLUE}Listing k3d clusters${NC}"
+sudo k3d cluster list
 
 # Check the version of kubectl
+echo "${BLUE}Checking the version of kubectl${NC}"
 kubectl version --client
--- p3/setup/2-init-argocd.sh
+++ p3/setup/2-init-argocd.sh
@@ -1,8 +1,40 @@
+#Colors
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+BLUE='\033[0;34m'
+
 # Desc: Install ArgoCD
-wget https://raw.githubusercontent.com/argoproj/argo-cd/v2.0.1/manifests/install.yaml
-# Desc: Create namespace for ArgoCD
-sudo kubectl create namespace argocd
-sudo kubectl create -n argocd -f install.yaml
+# echo "${BLUE}Installing ArgoCD${NC}"
+# wget https://raw.githubusercontent.com/argoproj/argo-cd/v2.0.1/manifests/install.yaml
+
+export KUBEVIRT_CONTEXT="lelhlami"
+sudo kubectl config use-context ${KUBEVIRT_CONTEXT}
+
+# Crete namespace DEV
+if [ -z "$(sudo kubectl get namespace dev)" ]; then
+    echo "${BLUE}Creating namespace DEV${NC}"
+    sudo kubectl create namespace dev
+    sudo kubectl apply -f configuration/app.yaml -n dev
+else
+    echo "${GREEN}The namespace dev already exists${NC}"
+fi
+# Create namespace for ArgoCD
+if [ -z "$(sudo kubectl get namespace argocd)" ]; then
+    echo "${BLUE}Creating namespace for ArgoCD${NC}"
+    sudo kubectl create namespace argocd
+    sudo kubectl create -n argocd -f configuration/install.yaml
+else
+    echo "${GREEN}The namespace argocd already exists${NC}"
+fi
 
 # Desc: Create an Ingress for ArgoCD
-sudo kubectl apply -f configuration/ingress.yaml -n argocd
+# echo "${BLUE}Creating an Ingress for ArgoCD${NC}"
+# sudo kubectl apply -f configuration/ingress.yaml -n argocd
+
+# Port forward ArgoCD
+echo "${BLUE}Creating an Ingress for ArgoCD${NC}"
+sudo kubectl port-forward svc/argocd-server -n argocd 8080:443
+
+alias k=kubectl
