2c728cc0b3b812ea3ced677da4c456492b1ce3de
E-lahcen <lelhlami@student.1337.ma>
2025-03-22 11:19:48 +0000

    Update p3

--- p3/setup/2-init-argocd.sh
+++ p3/setup/2-init-argocd.sh
@@ -9,13 +9,14 @@ BLUE='\033[0;34m'
 # echo "${BLUE}Installing ArgoCD${NC}"
 # wget https://raw.githubusercontent.com/argoproj/argo-cd/v2.0.1/manifests/install.yaml
 
-export KUBEVIRT_CONTEXT="lelhlami"
-sudo kubectl config use-context ${KUBEVIRT_CONTEXT}
+# export KUBEVIRT_CONTEXT="lelhlami"
+# sudo kubectl config use-context ${KUBEVIRT_CONTEXT}
 
 # Crete namespace DEV
 if [ -z "$(sudo kubectl get namespace dev)" ]; then
     echo "${BLUE}Creating namespace DEV${NC}"
     sudo kubectl create namespace dev
+    sudo kubectl apply -f app/deployment.yaml -n dev
 else
     echo "${GREEN}The namespace dev already exists${NC}"
 fi
@@ -25,7 +26,6 @@ if [ -z "$(sudo kubectl get namespace argocd)" ]; then
     sudo kubectl create namespace argocd
     sudo kubectl create -n argocd -f configuration/install.yaml
     sudo kubectl apply -f configuration/app.yaml -n argocd
-    sudo kubectl apply -f app/deployment.yaml -n argocd
 else
     echo "${GREEN}The namespace argocd already exists${NC}"
 fi
