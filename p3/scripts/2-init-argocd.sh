d58b5bf7cc2a88c2f94de27a79a3c745c61fe34e
E-lahcen <lelhlami@student.1337.ma>
2025-03-25 11:54:42 +0000

    Update deployment v2 no test

--- p3/scripts/2-init-argocd.sh
+++ p3/scripts/2-init-argocd.sh
@@ -38,6 +38,8 @@ fi
 # Port forward ArgoCD
 echo "${BLUE}Creating an Ingress for ArgoCD${NC}"
 
+sudo kubectl config use-context lelhlami
+
 # Wait for argocd pods to start
 echo "waiting for argocd pods to start.."
 echo "${GREEN}This may take a while${NC}"
@@ -52,14 +54,13 @@ printf "${GREEN}[ARGOCD]${NC} - Retrieving credentials...\n"
 
 echo "Login: admin, password: ${GREEN}$password"
 
-
 # sudo kubectl port-forward svc/argocd-server -n argocd 8080:80
 # sudo kubectl port-forward svc/argocd-server -n argocd 8080:443 --address="0.0.0.0" 2>&1 > /var/log/argocd-log &
 sudo kubectl port-forward svc/argocd-server -n argocd 8080:80 --address="0.0.0.0" > /dev/null 2>&1 &
 printf "${GREEN}[ARGOCD]${NC} - ArgoCD is running on http://localhost:8080\n"
 
 # sudo lsof -i:8888 -t | sudo xargs kill -9
-sudo kubectl port-forward svc/iot-svc -n dev 8888 --address="0.0.0.0" > /dev/null 2>&1 &
+sudo kubectl port-forward services/iot-svc 8888 -n dev --address="0.0.0.0" 2>&1 > /dev/null &
 printf "${GREEN}[DEV]${NC} - IoT service is running on http://localhost:8888\n"
 
 # while true; do
